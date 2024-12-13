bdb568fab8cc2f9d93dcba875ac89f4bf076505e
unknown <ivan.nikalayeu@treehousetechgroup.com>
2024-12-13 14:30:44 +0100

    temp

--- dags/test_spark_dag.py
+++ dags/test_spark_dag.py
@@ -23,18 +23,18 @@ with DAG(
     process_weather_data = SparkSubmitOperator(
         task_id='process_weather_data',
         # application='/scripts/process_weather_data.py',
-        application='s3a://scripts/process_weather_data.py',
+        application='/opt/airflow/dags/scripts/process_weather_data.py',
         name='process_weather_data_job',
         conn_id='spark_conn',
         verbose=True,
-        conf={
-            'spark.executor.memory': '2g',
-            'spark.executor.cores': '1',
-            'spark.hadoop.fs.s3a.access.key': 'uchBfIlUp1I8QUa8erMz',
-            'spark.hadoop.fs.s3a.secret.key': 'RQF7k9yXj9GIKVVN2ew3NmoDG0PTiJGUVDdfjlfd',
-            'spark.hadoop.fs.s3a.endpoint': 'minio-service:9000',
-            'spark.hadoop.fs.s3a.path.style.access': 'true'
-            },
+        # conf={
+        #     'spark.executor.memory': '2g',
+        #     'spark.executor.cores': '1',
+        #     'spark.hadoop.fs.s3a.access.key': 'uchBfIlUp1I8QUa8erMz',
+        #     'spark.hadoop.fs.s3a.secret.key': 'RQF7k9yXj9GIKVVN2ew3NmoDG0PTiJGUVDdfjlfd',
+        #     'spark.hadoop.fs.s3a.endpoint': 'minio-service:9000',
+        #     'spark.hadoop.fs.s3a.path.style.access': 'true'
+        #     },
 
         # env_vars={
         # "JAVA_HOME": "/usr/lib/jvm/java-11-openjdk",
@@ -42,6 +42,6 @@ with DAG(
         # },
         # application_args=['arg1', 'arg2'],
         executor_cores=2,
-        executor_memory='4g',
-        driver_memory='4g',
+        executor_memory='1g',
+        driver_memory='1g',
     )
