0a4628686aa6da054a1ebe1ab1af56b226fec2cd
unknown <ivan.nikalayeu@treehousetechgroup.com>
2024-12-13 11:06:53 +0100

    changed saving pyspark files

--- dags/test_spark_dag.py
+++ dags/test_spark_dag.py
@@ -22,15 +22,24 @@ with DAG(
     
     process_weather_data = SparkSubmitOperator(
         task_id='process_weather_data',
-        application='/scripts/process_weather_data.py',
+        # application='/scripts/process_weather_data.py',
+        application='s3a://scripts/process_weather_data.py',
         name='process_weather_data_job',
         conn_id='spark_conn',
         verbose=True,
-        conf={'spark.executor.memory': '2g', 'spark.executor.cores': '1'},
-        env_vars={
-        "JAVA_HOME": "/usr/lib/jvm/java-11-openjdk",
-        "PATH": "/usr/lib/jvm/java-11-openjdk/bin:$PATH"
-        },
+        conf={
+            'spark.executor.memory': '2g',
+            'spark.executor.cores': '1',
+            'spark.hadoop.fs.s3a.access.key': '3i8IlVsrgztIn0FxMXzO',
+            'spark.hadoop.fs.s3a.secret.key': 'iwJ2eueAMMklvOaxwPGsM8CXe8ty0vLETRHAmfoq',
+            'spark.hadoop.fs.s3a.endpoint': 'minio-service:9000',
+            'spark.hadoop.fs.s3a.path.style.access': 'true'
+            },
+
+        # env_vars={
+        # "JAVA_HOME": "/usr/lib/jvm/java-11-openjdk",
+        # "PATH": "/usr/lib/jvm/java-11-openjdk/bin:$PATH"
+        # },
         # application_args=['arg1', 'arg2'],
         executor_cores=2,
         executor_memory='4g',
