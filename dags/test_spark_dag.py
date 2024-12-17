370357f85eed6c00b96d66f37e75e1633bd0d68a
unknown <ivan.nikalayeu@treehousetechgroup.com>
2024-12-17 10:22:29 +0100

    tst

--- dags/test_spark_dag.py
+++ dags/test_spark_dag.py
@@ -26,20 +26,12 @@ with DAG(
         application='/opt/airflow/dags/repo/dags/scripts/process_weather_data.py',
         name='process_weather_data_job',
         conn_id='spark_conn',
+        spark_binary='/opt/spark/bin/spark-submit',
         # conf={
         #     "spark.kubernetes.container.image": "bitnami/spark:latest",  # Образ Spark
         #     "spark.kubernetes.namespace": "default",  # Namespace для Spark
         # },
         verbose=True,
-        # conf={
-        #     'spark.executor.memory': '2g',
-        #     'spark.executor.cores': '1',
-        #     'spark.hadoop.fs.s3a.access.key': 'uchBfIlUp1I8QUa8erMz',
-        #     'spark.hadoop.fs.s3a.secret.key': 'RQF7k9yXj9GIKVVN2ew3NmoDG0PTiJGUVDdfjlfd',
-        #     'spark.hadoop.fs.s3a.endpoint': 'minio-service:9000',
-        #     'spark.hadoop.fs.s3a.path.style.access': 'true'
-        #     },
-
         env_vars={
         "JAVA_HOME": "/usr/lib/jvm/java-11-openjdk",
         "PATH": "/usr/lib/jvm/java-11-openjdk/bin:$PATH"
